9ef5fdf6faffa4805ef8cc4b4bee047362908891
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-07 19:03:53 +0200

    test: run response tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ names = [
     "test_feedback.jl",
     "test_translate.jl",
     "test_correlations.jl",
+    "test_response.jl",
     "test_example_cavity_scattering.jl",
     "test_compare_example_05_1_05_2.jl",
     "test_interaction_picture.jl",
